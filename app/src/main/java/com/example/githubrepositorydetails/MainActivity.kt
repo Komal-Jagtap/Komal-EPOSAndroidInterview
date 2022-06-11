93cf1464bc10d916b2a8abeabb722805514e80a8
komal kakade <jagkomal@gmail.com>
2022-06-11 17:09:43 +0200

    Added github detail API

--- app/src/main/java/com/example/githubrepositorydetails/MainActivity.kt
+++ app/src/main/java/com/example/githubrepositorydetails/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.githubrepositorydetails
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.View
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.githubrepositorydetails.databinding.ActivityMainBinding
 import com.example.githubrepositorydetails.model.GitHubDataHolder
 import com.example.githubrepositorydetails.interfaces.ApiInterface
-
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -17,7 +20,6 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var viewModel: GitHubDataHolder
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,10 +27,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        getGithubRepository(this);
+        if (checkForInternet(this)) {
+            binding.relMain.visibility = View.VISIBLE
+            binding.relInternet.visibility = View.GONE
+            getGithubRepository(this)
+        } else {
+            binding.relMain.visibility = View.GONE
+            binding.relInternet.visibility = View.VISIBLE
+
+        }
     }
 
-    fun getGithubRepository(mainActivity: MainActivity) {
+    private fun getGithubRepository(mainActivity: MainActivity) {
         val apiInterface = ApiInterface.create().getGithubRepository()
         apiInterface.enqueue(object : Callback<List<GitHubDataHolder>> {
             override fun onResponse(
@@ -37,33 +47,33 @@ class MainActivity : AppCompatActivity() {
             ) {
                 Log.d("TAG", "@@@@ Respose:$response")
                 if (response != null) {
-                    Log.d("res", response.body().toString());
+                    Log.d("res", response.body().toString())
                     val items = response.body()
                     if (items != null) {
                         for (i in 0 until items.count()) {
-                            val name = items[i].name ?: "N/A"
+                            val name = items[i].name
 
-                            if (name.equals("facebook-android-sdk")) {
-                                val id = items[i].id ?: "N/A"
+                            if (name == "facebook-android-sdk") {
+                                val id = items[i].id
                                 val owner = items[i].owner
-                                val avatar_url = owner.component1()
-                                val forks_count = items[i].forks_count ?: "N/A"
-                                val open_issues_count = items[i].open_issues_count ?: "N/A"
-                                val language = items[i].language ?: "N/A"
+                                val avatarurl = owner.component1()
+                                val forkscount = items[i].forks_count
+                                val openissuescount = items[i].open_issues_count
+                                val language = items[i].language
                                 Log.d(
                                     "TAG",
-                                    "ID: $id name:$name language:$language forks_count:$forks_count avatar_url:$avatar_url"
+                                    "ID: $id name:$name language:$language forks_count:$forkscount avatar_url:$avatarurl"
                                 )
 
                                 binding.tvRepositoryName.setText(name)
-                                binding.tvForks.setText(forks_count)
+                                binding.tvForksValue.setText(forkscount)
                                 binding.tvRepositoryLanguage.setText(language)
-                                binding.tvIssuesValue.setText(open_issues_count)
+                                binding.tvIssuesValue.setText(openissuescount)
 
-                                Log.d("TAG", "owner: $avatar_url")
+                                Log.d("TAG", "owner: $avatarurl")
 
                                 Glide.with(mainActivity)
-                                    .load(avatar_url)
+                                    .load(avatarurl)
                                     .into(binding.ivRepositoryLogo)
                                 break
                             }
@@ -76,5 +86,45 @@ class MainActivity : AppCompatActivity() {
 
             }
         })
+    }
+
+    private fun checkForInternet(context: Context): Boolean {
+
+        // register activity with the connectivity manager service
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        // if the android version is equal to M
+        // or greater we need to use the
+        // NetworkCapabilities to check what type of
+        // network has the internet connection
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            // Returns a Network object corresponding to
+            // the currently active default data network.
+            val network = connectivityManager.activeNetwork ?: return false
+
+            // Representation of the capabilities of an active network.
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+            return when {
+                // Indicates this network uses a Wi-Fi transport,
+                // or WiFi has network connectivity
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+
+                // Indicates this network uses a Cellular transport. or
+                // Cellular has network connectivity
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+
+                // else return false
+                else -> false
+            }
+        } else {
+            // if the android version is below M
+            @Suppress("DEPRECATION") val networkInfo =
+                connectivityManager.activeNetworkInfo ?: return false
+            @Suppress("DEPRECATION")
+            return networkInfo.isConnected
+        }
     }
 }
